cabbb2126a96be47e4ba19a181af2bb4221768da
ilya.podshivalov <ipodshivalov@getmobit.ru>
2018-12-11 16:49:27 +0300

    Add some tests

--- tests/test_version.cpp
+++ tests/test_version.cpp
@@ -1,8 +1,8 @@
-#define BOOST_TEST_MODULE helloworld_test_module
+#define BOOST_TEST_MODULE matrix_test_module
 #include "../crs/lib.h"
 #include <boost/test/unit_test.hpp>
-BOOST_AUTO_TEST_SUITE(helloworld_test_suite)
-BOOST_AUTO_TEST_CASE(helloworld_test_version)
+BOOST_AUTO_TEST_SUITE(matrix_test_suite)
+BOOST_AUTO_TEST_CASE(matrix_test_version)
 {
     BOOST_CHECK(versionMajor() >= 0);
     BOOST_CHECK(versionMinor() >= 0);
