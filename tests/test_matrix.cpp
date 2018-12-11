412a52e7a794c5d99bff709b76ac70736719d2a2
ilya.podshivalov <ipodshivalov@getmobit.ru>
2018-12-11 17:09:26 +0300

    Try to commit the latest test

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -35,18 +35,18 @@ BOOST_AUTO_TEST_CASE(matrix_test_cell)
     BOOST_CHECK_EQUAL(val, 1);
 }
 
-//BOOST_AUTO_TEST_CASE(matrix_test_canonical_eq)
-//{
-//    Matrix<int, -7> matrix;
-//    BOOST_CHECK_EQUAL(matrix.size(), 0);
-//    matrix[12][11]=-7;
-//    BOOST_CHECK_EQUAL(matrix.size(), 0);
-//    (matrix[12][11]=-7)=0;
-//    BOOST_CHECK_EQUAL(matrix.size(), 1);
-//    ((matrix[12][11]=-7)=0)=-7;
-//    BOOST_CHECK_EQUAL(matrix.size(), 0);
-//    (((matrix[12][11]=-7)=0)=-7)=1231;
-//    BOOST_CHECK_EQUAL(matrix[12][11], 1231);
-//}
+BOOST_AUTO_TEST_CASE(matrix_test_canonical_eq)
+{
+    Matrix<int, -7> matrix;
+    BOOST_CHECK_EQUAL(matrix.size(), 0);
+    matrix[12][11]=-7;
+    BOOST_CHECK_EQUAL(matrix.size(), 0);
+    (matrix[12][11]=-7)=0;
+    BOOST_CHECK_EQUAL(matrix.size(), 1);
+    ((matrix[12][11]=-7)=0)=-7;
+    BOOST_CHECK_EQUAL(matrix.size(), 0);
+    (((matrix[12][11]=-7)=0)=-7)=1231;
+    BOOST_CHECK_EQUAL(matrix[12][11], 1231);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
