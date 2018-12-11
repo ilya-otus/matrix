df962c17347f1d0fda8741e6fda7b67f9260757a
ilya.podshivalov <ipodshivalov@getmobit.ru>
2018-12-11 17:06:13 +0300

    Try to find out which test doesn't pass on CI

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -19,22 +19,22 @@ BOOST_AUTO_TEST_CASE(matrix_test_count)
     BOOST_CHECK_EQUAL(matrix.size(), 1);
 }
 
-//BOOST_AUTO_TEST_CASE(matrix_test_cell)
-//{
-//    Matrix<int, -7> matrix;
-//    matrix[12][11]=1;
-//    matrix[10][12]=-7;
-//    auto cell = *(matrix.begin());
-//    BOOST_CHECK_EQUAL(matrix.size(), 1);
-//    size_t row, col;
-//    int val;
-//    std::tie(row, col, val) = cell;
-//    BOOST_CHECK_EQUAL(matrix.size(), 1);
-//    BOOST_CHECK_EQUAL(row, 12);
-//    BOOST_CHECK_EQUAL(col, 11);
-//    BOOST_CHECK_EQUAL(val, 1);
-//}
-//
+BOOST_AUTO_TEST_CASE(matrix_test_cell)
+{
+    Matrix<int, -7> matrix;
+    matrix[12][11]=1;
+    matrix[10][12]=-7;
+    auto cell = *(matrix.begin());
+    BOOST_CHECK_EQUAL(matrix.size(), 1);
+    size_t row, col;
+    int val;
+    std::tie(row, col, val) = cell;
+    BOOST_CHECK_EQUAL(matrix.size(), 1);
+    BOOST_CHECK_EQUAL(row, 12);
+    BOOST_CHECK_EQUAL(col, 11);
+    BOOST_CHECK_EQUAL(val, 1);
+}
+
 //BOOST_AUTO_TEST_CASE(matrix_test_canonical_eq)
 //{
 //    Matrix<int, -7> matrix;
